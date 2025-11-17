ab3f8286cc7efeb76a1d2c17b1d0d77fc252c0e7
mmendoza20 <miguelmendoza1982@live.com>
2025-11-17 15:12:50 -0800

    C: Added ship, asteroids, and their movement logic

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
 from logger import log_state
 
-pygame.init()
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    timer = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    ast_field = AsteroidField()
 
-    while 1 != 0:
+    
+
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 
 if __name__ == "__main__":
